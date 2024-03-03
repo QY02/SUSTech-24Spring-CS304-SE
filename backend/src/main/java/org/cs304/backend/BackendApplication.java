f3e0beb02a5dab96227d5ea00679b0c9bdfec166
QY02 <1494893742@qq.com>
2024-03-03 22:52:32 +0800

    add swagger and some configs

--- backend/src/main/java/org/cs304/backend/BackendApplication.java
+++ backend/src/main/java/org/cs304/backend/BackendApplication.java
@@ -2,12 +2,16 @@ package org.cs304.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(BackendApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("app.pid"));
+        ConfigurableApplicationContext configurableApplicationContext = springApplication.run(args);
     }
 
 }
