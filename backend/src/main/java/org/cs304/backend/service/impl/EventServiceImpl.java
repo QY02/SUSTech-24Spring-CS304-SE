0bf43411e273f532cac60fd93528d9c019d652d6
ZhangYP21 <2826287819@qq.com>
2024-03-13 23:15:56 +0800

    frontend: 活动申请表 backend:

--- backend/src/main/java/org/cs304/backend/service/impl/EventServiceImpl.java
+++ backend/src/main/java/org/cs304/backend/service/impl/EventServiceImpl.java
@@ -1,12 +1,43 @@
 package org.cs304.backend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
 import org.cs304.backend.entity.Event;
 import org.cs304.backend.mapper.EventMapper;
 import org.cs304.backend.service.IEventService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventServiceImpl extends ServiceImpl<EventMapper, Event> implements IEventService {
+    @Autowired
+    private EventMapper eventMapper;
 
+    // 插入新事件
+    public void insertEvent(Event event) {
+        eventMapper.insert(event);
+    }
+
+    // 更新事件
+    public void updateEvent(Event event) {
+        eventMapper.updateById(event);
+    }
+
+    // 删除事件
+    public void deleteEvent(Long eventId) {
+        eventMapper.deleteById(eventId);
+    }
+
+    // 查询事件
+    public Event getEventById(Long eventId) {
+        return eventMapper.selectById(eventId);
+    }
+
+    // 根据条件查询事件列表
+    public List<Event> listEvents(QueryWrapper<Event> wrapper) {
+        return eventMapper.selectList(wrapper);
+    }
 }
---
+++ frontend/src/components/Event/EventSession.vue
@@ -0,0 +1,433 @@
+<template>
+  <div class="t-table-demo__editable-row">
+    <div>
+      <t-button @click="()=>visibleBody=true">
+        <template #icon>
+          <add-icon/>
+        </template>
+        添加新场次
+      </t-button>
+    </div>
+    <br/>
+    <t-table
+        ref="tableRef"
+        row-key="key"
+        :columns="columns"
+        :rules="FORM_RULES"
+        :data="data"
+        stripe="true"
+        :editable-row-keys="editableRowKeys"
+        bordered="true"
+        :pagination="pagination"
+        lazy-load="true"
+        resizable="true"
+        @row-edit="onRowEdit"
+        @row-validate="onRowValidate"
+        @validate="onValidate"
+    />
+    <t-dialog
+        v-model:visible="visibleBody"
+        attach="body"
+        header="请填写场次信息"
+        destroy-on-close="true"
+        width="45%"
+        :cancel-btn="null"
+        :confirm-btn="null"
+    >
+      <template #body>
+        <t-space direction="vertical" size="large">
+          <t-form
+              ref="form"
+              id="form"
+              :data="newData"
+              reset-type="initial"
+              @reset="onReset"
+              @submit="addData"
+              :rules="FORM_RULES"
+              label-width="190px"
+          >
+            <t-form-item label="是否需要报名" name="registration_required">
+              <t-switch v-model="newData.registration_required" :label="['是', '否']"></t-switch>
+            </t-form-item>
+            <t-form-item label="报名开始时间-报名结束时间" name="registration_time_range">
+              <t-date-range-picker enable-time-picker="true" allow-input="true" clearable="true"
+                                   v-model="newData.registration_time_range"
+                                   :disabled="!newData.registration_required"/>
+            </t-form-item>
+            <t-form-item label="开始时间-结束时间" name="event_time_range">
+              <t-date-range-picker enable-time-picker="true" allow-input="true" clearable="true"
+                                   v-model="newData.event_time_range"/>
+            </t-form-item>
+
+            <t-form-item label="人数" name="count_range_of_people">
+              <t-range-input v-model="newData.count_range_of_people" :placeholder="['最小值','最大值']"/>
+            </t-form-item>
+            <t-form-item label="地址" name="venue">
+              <t-input v-model="newData.venue">地址</t-input>
+            </t-form-item>
+            <t-form-item label="地图" name="location">
+              <t-input v-model="newData.location">地图</t-input>
+            </t-form-item>
+            <t-form-item label="是否可见" name="visible">
+              <t-switch v-model="newData.visible" :label="['是', '否']"></t-switch>
+            </t-form-item>
+
+            <t-form-item>
+              <t-space size="small">
+                <t-button theme="primary" type="submit">提交</t-button>
+                <t-button theme="default" variant="base" type="reset">重置</t-button>
+              </t-space>
+            </t-form-item>
+          </t-form>
+        </t-space>
+
+      </template>
+    </t-dialog>
+
+  </div>
+</template>
+
+<script setup lang="jsx">
+import {computed, ref} from 'vue';
+import {DateRangePicker, Input, MessagePlugin, RangeInput, Switch} from 'tdesign-vue-next';
+import {AddIcon} from "tdesign-icons-vue-next";
+import React from 'react';
+
+
+let cnt = 1;
+const initData = new Array(7).fill(null).map(() => ({
+  key: String(cnt++),
+  registration_required: false,
+  registration_time_range: [],
+  event_time_range: [],
+  count_range_of_people: [],
+  seat_map_id: '',
+  venue: '',
+  location: '',
+  visible: false,
+}));
+const data = ref([...initData]);
+
+const newData = ref({
+  key: String(cnt++),
+  registration_required: false,
+  registration_time_range: [],
+  event_time_range: [],
+  count_range_of_people: [],
+  seat_map_id: '',
+  venue: '',
+  location: '',
+  visible: false,
+});
+
+const tableRef = ref();
+const editableRowKeys = ref([]);
+const currentSaveId = ref('');
+// 保存变化过的行信息
+const editMap = {};
+const visibleBody = ref(false);
+const pagination = computed(() => ({
+  defaultCurrent: 1,
+  defaultPageSize: 5,
+  total: data.value.length
+}));
+
+const onEdit = (e) => {
+  const {id} = e.currentTarget.dataset;
+  if (!editableRowKeys.value.includes(id)) {
+    editableRowKeys.value.push(id);
+  }
+};
+
+// 更新 editableRowKeys
+const updateEditState = (id) => {
+  const index = editableRowKeys.value.findIndex((t) => t === id);
+  editableRowKeys.value.splice(index, 1);
+};
+
+const onCancel = (e) => {
+  const {id} = e.currentTarget.dataset;
+  updateEditState(id);
+  tableRef.value.clearValidateData();
+};
+
+const onSave = (e) => {
+  const {id} = e.currentTarget.dataset;
+  currentSaveId.value = id;
+  // 触发内部校验，而后也可在 onRowValidate 中接收异步校验结果
+  tableRef.value.validateRowData(id).then((params) => {
+    console.log('Event Table Promise Validate:', params);
+    if (params.result.length) {
+      const r = params.result[0];
+      MessagePlugin.error(`${r.col.title} ${r.errorList[0].message}`);
+      return;
+    }
+    // 如果是 table 的父组件主动触发校验
+    if (params.trigger === 'parent' && !params.result.length) {
+      const current = editMap[currentSaveId.value];
+      if (current) {
+        alert(JSON.stringify(current.editedRow))
+        data.value.splice(current.rowIndex, 1, current.editedRow);
+        MessagePlugin.success('保存成功');
+      }
+      updateEditState(currentSaveId.value);
+    }
+  });
+};
+
+// 行校验反馈事件，tableRef.value.validateRowData 执行结束后触发
+const onRowValidate = (params) => {
+  console.log('Event Table Row Validate:', params);
+};
+
+const onDelete = (e) => {
+  const {id} = e.currentTarget.dataset;
+  // 删除操作
+  // 例如：根据 id 找到对应的行索引，然后使用 splice 方法将其从数据中移除
+  const index = data.value.findIndex((item) => item.key === id);
+  if (index !== -1) {
+    const temp = [...data.value];
+    temp.splice(index, 1);
+    data.value = temp;
+    MessagePlugin.success('删除成功');
+  } else {
+    MessagePlugin.error('未找到要删除的行');
+  }
+};
+const addData = ({validateResult, firstError}) => {
+  if (validateResult === true) {
+    data.value = [...data.value, newData.value]
+    newData.value = {
+      key: String(cnt++),
+      registration_required: false,
+      registration_time_range: [],
+      event_time_range: [],
+      count_range_of_people: [],
+      seat_map_id: '',
+      venue: '',
+      location: '',
+      visible: false,
+    }
+    visibleBody.value = false;
+    MessagePlugin.success('提交成功');
+  } else {
+    console.log('Validate Errors: ', firstError, validateResult);
+    MessagePlugin.warning(firstError);
+  }
+}
+
+function onValidate(params) {
+  console.log('Event Table Data Validate:', params);
+}
+
+const onRowEdit = (params) => {
+  const {row, col, value} = params;
+  const oldRowData = editMap[row.key]?.editedRow || row;
+  const editedRow = {...oldRowData, [col.colKey]: value};
+  editMap[row.key] = {
+    ...params,
+    editedRow,
+  };
+};
+
+
+const columns = computed(() => [
+  {
+    title: '序号', colKey: 'key',
+    width: 60, align: 'center',
+    fixed: 'left',
+    // cell: (h, {rowIndex}) => {
+    //   return <span>{rowIndex + 1}</span>;
+    // },
+  },
+  {
+    title: '是否需要报名', colKey: 'registration_required',
+    width: 120, align: 'center',
+    cell: (h, {row}) => {
+      // Convert registration_required value to display as '是' or '否'
+      const displayValue = row.registration_required === true ? '是' : '否';
+      return <span>{displayValue}</span>;
+    },
+    edit: {
+      component: Switch,
+      props: {
+        label: ['是', '否']
+      },
+      showEditIcon: false,
+      on: ({updateEditedCellValue}) => ({
+        onChange: (value) => {
+          // window.alert(value.value)
+          updateEditedCellValue({
+            isUpdateCurrentRow: true,
+            registration_required: value.value
+          });
+        },
+      }),
+    },
+  },
+  {
+    title: '申请时间', colKey: 'registration_time_range',
+    width: 400, align: 'center',
+    cell: (h, {row}) => {
+      let re = "无需预约"
+      if (row.registration_required) {
+        const displayValue = row.registration_time_range;
+        re = <span>{`${displayValue[0]} 到 ${displayValue[1]}`}</span>;
+      }
+      return re
+    },
+    edit: {
+      component: DateRangePicker, showEditIcon: false,
+      props: {
+        enableTimePicker: true
+      },
+    },
+  },
+  {
+    title: '活动日期', colKey: 'event_time_range',
+    width: 400, align: 'center',
+    edit: {
+      component: DateRangePicker, showEditIcon: false,
+      props: {
+        allowInput: true, enableTimePicker: true
+      },
+    },
+    cell: (h, {row}) => {
+      const displayValue = row.event_time_range;
+      return <span>{`${displayValue[0]} 到 ${displayValue[1]}`}</span>
+    },
+  },
+  {
+    title: '人数范围', colKey: 'count_range_of_people',
+    width: 200, align: 'center',
+    cell: (h, {row}) => {
+      const displayValue = row.count_range_of_people;
+      return <span>{`${displayValue[0]} ~ ${displayValue[1]}`}</span>
+    },
+    edit: {
+      component: RangeInput, showEditIcon: false,
+    },
+  },
+  {
+    title: '座位', colKey: 'seat_map_id',
+    width: 100, align: 'center',
+    edit: {
+      component: Input, showEditIcon: false,
+    },
+  },
+  {
+    title: '地点', colKey: 'venue',
+    width: 100, align: 'center',
+    edit: {
+      component: Input, showEditIcon: false,
+    },
+  },
+  {
+    title: '地图', colKey: 'location',
+    width: 100, align: 'center',
+    edit: {
+      component: Input, showEditIcon: false,
+    },
+  },
+  {
+    title: '是否可见', colKey: 'visible',
+    align: 'center',
+    cell: (h, {row}) => {
+      // Convert visible required value to display as '是' or '否'
+      const displayValue = row.visible === true ? '是' : '否';
+      return <span>{displayValue}</span>;
+    },
+    edit: {
+      component: Switch, showEditIcon: false,
+      props: {
+        label: ['是', '否']
+      },
+      on: ({updateEditedCellValue}) => ({
+        onChange: (value) => {
+          // window.alert(value.value)
+          updateEditedCellValue({
+            isUpdateCurrentRow: true,
+            visible: value.value
+          });
+        },
+      }),
+    },
+  },
+  {
+    title: '操作栏',
+    colKey: 'operate',
+    width: 150, align: 'center',
+    fixed: 'right',
+    cell: (h, {row}) => {
+      const editable = editableRowKeys.value.includes(row.key);
+      return (
+          <div className="table-operations">
+            {!editable && (
+                <div className="operations">
+                  <t-link theme="primary" hover="color" data-id={row.key} onClick={onEdit}>编辑</t-link>
+                  <t-link theme="danger" hover="color" data-id={row.key} onClick={onDelete}>删除</t-link>
+                </div>
+            )}
+            {editable && (
+                <div className="operations">
+                  <t-link theme="primary" hover="color" data-id={row.key} onClick={onSave}>保存</t-link>
+                  <t-link theme="primary" hover="color" data-id={row.key} onClick={onCancel}>取消</t-link>
+                </div>
+            )}
+          </div>
+      );
+    },
+  },
+]);
+
+const count_range_of_peopleValidator = (val) => {
+  // 将输入的字符串转化为数字
+  const [first, second] = val.map(Number);
+
+  if (isNaN(first) || first <= 0) {
+    return {result: false, message: '最小值应该为正数', type: 'error'};
+  }
+  if (isNaN(second) || second <= 0) {
+    return {result: false, message: '最大值应该为正数', type: 'error'};
+  }
+
+  // 检查第二个数字是否大于等于第一个数字
+  if (second < first) {
+    return {result: false, message: '最大值必须大于等于最小值', type: 'error'};
+  }
+
+  return {result: true};
+};
+
+const FORM_RULES = ref({
+  registration_time_range: [{required: computed(() => newData.value.registration_required), message: '报名时间必填'}],
+  event_time_range: [{required: true, message: '活动时间必填'}],
+  count_range_of_people: [
+    {required: true, message: '人数必填'},
+    {validator: count_range_of_peopleValidator},
+  ],
+  venue: [{required: true, message: '地址必填'}],
+  position: [{required: true, message: '地址必填'}],
+
+});
+const onReset = () => {
+  MessagePlugin.success('重置成功');
+};
+
+</script>
+
+<style lang="css">
+.t-table-demo__editable-row .table-operations > .t-link {
+  margin-right: 8px;
+}
+
+.t-table-demo__editable-row .t-demo-col__datepicker .t-date-picker {
+  width: 120px;
+}
+
+.operations {
+  display: flex;
+  justify-content: space-around;
+  gap: 10px;
+}
+</style>
