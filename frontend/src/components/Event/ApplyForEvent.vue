4949e7ead161d4c8364e5e1a1bca053df56e0de5
ultramarine <2549952274@qq.com>
2024-03-19 01:03:20 +0800

    eventPage-粗糙版完工

--- frontend/src/components/Event/ApplyForEvent.vue
+++ frontend/src/components/Event/ApplyForEvent.vue
@@ -1,20 +1,13 @@
 <template>
   <t-space direction="vertical" size="large">
     <b>请输入活动信息</b>
-    <t-form
-        ref="form"
-        :data="formData"
-        reset-type="initial"
-        @reset="onReset"
-        @submit="onSubmit"
-        :rules="FORM_RULES"
-    >
+    <t-form ref="form" :data="formData" reset-type="initial" @reset="onReset" @submit="onSubmit" :rules="FORM_RULES">
       <t-form-item label="标题" name="name">
         <t-input v-model="formData.name">标题</t-input>
       </t-form-item>
 
       <t-form-item label="简介" name="content">
-        <t-textarea v-model="formData.content" placeholder="简单描述项目内容" clearable/>
+        <t-textarea v-model="formData.content" placeholder="简单描述项目内容" clearable />
       </t-form-item>
 
       <t-form-item label="类型" name="type">
@@ -27,18 +20,13 @@
       </t-form-item>
 
       <t-form-item label="海报" name="poster">
-        <t-upload
-            v-model="formData.poster"
-            action="https://service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo"
-            theme="image"
-            tips="请选择单张图片文件上传"
-            accept="image/*"
-        ></t-upload>
+        <t-upload v-model="formData.poster"
+          action="https://service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo" theme="image"
+          tips="请选择单张图片文件上传" accept="image/*"></t-upload>
       </t-form-item>
-      <div style="max-width: 900px;"
-      >
-                <event-session></event-session>
-      </div>
+      <div style="max-width: 900px;">
+        <event-session></event-session>
+      </div>œ
 
       <t-form-item>
         <t-space size="small">
@@ -50,8 +38,8 @@
   </t-space>
 </template>
 <script setup>
-import {ref, inject} from 'vue';
-import {MessagePlugin} from 'tdesign-vue-next';
+import { ref, inject } from 'vue';
+import { MessagePlugin } from 'tdesign-vue-next';
 import axios from "axios";
 import SessionEvent from "@/components/Event/EventSession.vue";
 import TempTry from "@/components/Event/TempTry.vue";
@@ -61,10 +49,10 @@ const apiUrl = inject('$API_URL');
 
 
 const FORM_RULES = {
-  name: [{required: true, message: '标题必填'}],
-  content: [{required: true, message: '简介必填'}],
-  type: [{required: true, message: '类型必填'}],
-  poster: [{required: true, message: '必须上传海报'}],
+  name: [{ required: true, message: '标题必填' }],
+  content: [{ required: true, message: '简介必填' }],
+  type: [{ required: true, message: '类型必填' }],
+  poster: [{ required: true, message: '必须上传海报' }],
 };
 
 
@@ -72,7 +60,7 @@ const formData = ref({
   name: '',
   content: '',
   type: '',
-  poster: [{url: 'https://tdesign.gtimg.com/site/avatar.jpg'}],
+  poster: [{ url: 'https://tdesign.gtimg.com/site/avatar.jpg' }],
 });
 
 
@@ -80,13 +68,13 @@ const onReset = () => {
   MessagePlugin.success('重置成功');
 };
 
-const onSubmit = ({validateResult, firstError}) => {
+const onSubmit = ({ validateResult, firstError }) => {
   if (validateResult === true) {
     // console.log(formData)
     axios.post(`${apiUrl}/event/add`).then(
-        response => {
-          console.log(response)
-        }
+      response => {
+        console.log(response)
+      }
     ).catch(error => alert(error))
     MessagePlugin.success('提交成功');
   } else {
@@ -95,4 +83,3 @@ const onSubmit = ({validateResult, firstError}) => {
   }
 };
 </script>
-
--- frontend/src/components/Event/EventPage.vue
+++ frontend/src/components/Event/EventPage.vue
@@ -12,12 +12,12 @@
   <div class="right">
     <div class="card-2">
       <h2>标题</h2>
-      </div>
-      <div class="card-3">
-        it's me hi
-        {{ infoMessage }}
-      </div>
-      <div class="bottom">
+    </div>
+    <div class="card-3">
+      it's me hi
+      {{ infoMessage }}
+    </div>
+    <div class="bottom">
     <div class="card-4">
     <t-space size="24px">
       <t-button shape="circle" theme="primary" @click="navigateToTab('review')">
@@ -38,69 +38,198 @@
   </div>
 </div>
 
+
+
+
   <el-affix :offset="55">
     <t-tabs :value="value_lable" size="large" @change="onTabChange" :affix-props="{ offsetTop: 150 }">
-        <t-tab-panel value="events" label="EVENTS"></t-tab-panel>
-        <t-tab-panel value="about"  label="ABOUT"></t-tab-panel>
-        <t-tab-panel value="review" label="REVIEW"></t-tab-panel>
-      </t-tabs>
+            <t-tab-panel value="events" label="EVENTS"></t-tab-panel>
+            <t-tab-panel value="about" label="EVENT DETAILS"></t-tab-panel>
+            <t-tab-panel value="price" label="TICKET PRICING"></t-tab-panel>
+            <t-tab-panel value="reviews" label="REVIEWS"></t-tab-panel>
+            <t-tab-panel value="policy" label="ADMISSION POLICY"></t-tab-panel>
+    </t-tabs>
   </el-affix>
   <div :id="`${path}#events`" style="height: 60px;"></div>
-  <h2>EVENTS</h2>
-  
-  <div :id="`${path}#about`" style="height: 60px;"></div>
-  <div>
-      Taylor Swift in Concert
-  
-  She is, quite simply, a global superstar.
-    
-  Taylor Swift is a seven-time GRAMMY winner, and the youngest recipient in history of the music industry’s highest honor, the GRAMMY Award for Album of the Year. She is the only female artist in music history (and just the fourth artist ever) to twice have an album hit the 1 million first-week sales figure (2010’s Speak Now and 2012’s RED). She’s a household name whose insanely catchy yet deeply personal self-penned songs transcend music genres, and a savvy businesswoman who has built a childhood dream into an empire.
-    
-  But the numbers don’t tell Taylor's story half as well as she could. After all, it’s the intangibles that elevate Swift into the stratosphere of our pop culture planet, allowing the 24-year old singer-songwriter to orbit in a more rarified air. Her large-scale charitable contributions are one thing, but it’s in the small gestures – the notes of compassion she posts on the Instagram photos of lovelorn fans, the genuine hugs she distributes without discretion – where Swift proves time and time again that platinum-selling, record-setting success has not changed her inherent nature. She is awkwardly honest and powerfully empathetic; a brazen superfan, loyal friend, fierce protector of hearts; and one of the world’s greatest ambassadors for the power of just being yourself.
-    
-  Granted, for Taylor, “being herself” tends towards shimmering, gossamer perfection – but that’s an image regularly blown whenever she dons fake braces and a tri-pony to clown around on late night TV. She’s the first artist since the Beatles (and the only female artist in history) to log six or more weeks at #1 with three consecutive studio albums, and while she’s been named one of Time magazine’s 100 Most Influential People in the World, she’s probably the only person on that list who uses social media to post notes to her best friends and videos of her cats. 
-    
-  As Billboard’s only artist to receive this nod twice and youngest-ever Woman of the Year prepares to release her fifth album, 1989, she finds herself, as always, in the glare of a blinding spotlight of expectation – but if you think that scares her, you haven’t been paying attention. She calls 1989 her most sonically cohesive collection, and armed with a multiple-week international No. 1 debut single, “Shake It Off,” she’s ready to blaze into the next phase of her still-young career, where she’ll continue to dance like no one’s watching, write like she stole our collective diary, and inevitably soar to ever-greater heights. All that’s left to wonder is how many more lives she’ll lift in the process.
-  
 
-  </div>
-
-  <div :id="`${path}#review`" style="height: 60px;"></div>
-  <div>
-    <t-list :split="true">
-      <t-list-item v-for="(item, index) in commentsData" :key="index">
-        <template #content>
-          <t-comment :avatar="item.avatar" :author="item.author" :datetime="item.datetime" :content="item.content">
-            <template #actions>
-              <t-space key="thumbUp" :size="6">
-                <t-icon name="thumb-up" />
-                <span>6</span>
-              </t-space>
-              <t-space key="chat" :size="6">
-                <t-icon name="chat" />
-                <span>回复</span>
-              </t-space>
-            </template>
-          </t-comment>
-          <div>
-            <h3>评分</h3>
-            <t-rate :default-value="4.5" allow-half disabled size="16" />
+  <t-space style="display: flex; width: 100%; margin-left: 24px;">
+      <div>
+        <div class="title">EVENTS</div>
+        <div class="line"></div>
+      </div>
+      <div style="margin-top: 30px; display: flex; justify-content: flex-end; margin-right:79px ;">
+          <div v-if="show_event_type">
+            <t-space>
+              <t-button theme="primary" shape="square" variant="base" @click="onClickEventType(true)">
+                <list-icon slot="icon" />
+              </t-button>
+              <t-button shape="square" variant="outline" @click="onClickEventType(false)">
+                <table-icon slot="icon" />
+              </t-button>
+            </t-space>
           </div>
-        </template>
-      </t-list-item>
-    </t-list>
+          <div v-else>
+            <t-space>
+              <t-button shape="square" variant="outline" @click="onClickEventType(true)">
+                <list-icon slot="icon" />
+              </t-button>
+              <t-button  theme="primary" shape="square" variant="base" @click="onClickEventType(false)">
+                <table-icon slot="icon" />
+              </t-button>
+            </t-space>
+          </div>
+      </div>
+  </t-space>
+  <el-card style="height:720px; padding: 5px;  max-width: 100% ; margin-right: 30px; margin-left: 30px; margin-bottom: 40px;">
+    <div v-if="show_event_type">
+      <EventTicketTable></EventTicketTable>
+    </div>
+    <div v-else>
+      <EventTicketCalender></EventTicketCalender>
+    </div>
+  </el-card>
+  <div style="height: 20px;"></div>
+  <div :id="`${path}#about`" style="height: 36px;"></div>
+  <div class="card" style="flex-direction: column;">
+    <t-space style="display: flex; width: 100%;">
+      <div>
+          <div class="title">EVENT DETAILS</div>
+          <div class="line"></div>
+        </div>
+    </t-space>
+    <div style="margin-left: 25px;margin-right: 25px; margin-bottom: 40px;">
+      Taylor Swift in Concert
+        
+        She is, quite simply, a global superstar.
+          
+        Taylor Swift is a seven-time GRAMMY winner, and the youngest recipient in history of the music industry’s highest honor, the GRAMMY Award for Album of the Year. She is the only female artist in music history (and just the fourth artist ever) to twice have an album hit the 1 million first-week sales figure (2010’s Speak Now and 2012’s RED). She’s a household name whose insanely catchy yet deeply personal self-penned songs transcend music genres, and a savvy businesswoman who has built a childhood dream into an empire.
+          
+        But the numbers don’t tell Taylor's story half as well as she could. After all, it’s the intangibles that elevate Swift into the stratosphere of our pop culture planet, allowing the 24-year old singer-songwriter to orbit in a more rarified air. Her large-scale charitable contributions are one thing, but it’s in the small gestures – the notes of compassion she posts on the Instagram photos of lovelorn fans, the genuine hugs she distributes without discretion – where Swift proves time and time again that platinum-selling, record-setting success has not changed her inherent nature. She is awkwardly honest and powerfully empathetic; a brazen superfan, loyal friend, fierce protector of hearts; and one of the world’s greatest ambassadors for the power of just being yourself.
+          
+        Granted, for Taylor, “being herself” tends towards shimmering, gossamer perfection – but that’s an image regularly blown whenever she dons fake braces and a tri-pony to clown around on late night TV. She’s the first artist since the Beatles (and the only female artist in history) to log six or more weeks at #1 with three consecutive studio albums, and while she’s been named one of Time magazine’s 100 Most Influential People in the World, she’s probably the only person on that list who uses social media to post notes to her best friends and videos of her cats. 
+          
+        As Billboard’s only artist to receive this nod twice and youngest-ever Woman of the Year prepares to release her fifth album, 1989, she finds herself, as always, in the glare of a blinding spotlight of expectation – but if you think that scares her, you haven’t been paying attention. She calls 1989 her most sonically cohesive collection, and armed with a multiple-week international No. 1 debut single, “Shake It Off,” she’s ready to blaze into the next phase of her still-young career, where she’ll continue to dance like no one’s watching, write like she stole our collective diary, and inevitably soar to ever-greater heights. All that’s left to wonder is how many more lives she’ll lift in the process.
+        
+    </div>
   </div>
+
+
+  <div :id="`${path}#price`" style="height: 60px;"></div>
+  <t-space style="display: flex; width: 100%; margin-left: 24px;">
+      <div>
+        <div class="title">TICKET PRICING</div>
+        <div class="line"></div>
+      </div>
+  </t-space>
+  <div class="ticket_card" style="margin-left: 55px; margin-top: -10px;">
+      <p style="  color: rgba(7, 63, 216, 1); font-size: 18px; font-weight: 700; letter-spacing: 1px;">
+        GENERAL SALE
+      </p>
+      <p style="  margin-top: 0.4rem; color: rgb(70, 73, 79);font-weight: 600;">
+        开始日期
+      </p>
+      <p style=" margin-top: -5px; line-height: 1.625; color: rgb(70, 73, 79);">
+        2024年3月14号<br>13:00
+      </p>
+      <el-divider />
+      <p style="  color: rgba(7, 63, 216, 1); font-size: 18px; font-weight: 700; letter-spacing: 1px;">
+        STANDARD
+      </p>
+      <p style="  margin-top: 0.4rem; line-height: 1.625; color: rgb(70, 73, 79);;">
+        CAT1: ¥888
+        <br>
+        CAT2: ¥688
+        <br>
+        CAT3: ¥488
+        <br>
+        CAT4: ¥388
+        <br>
+        CAT5: ¥288
+        <br>
+      </p>
+  </div>
+  <div style="height: 20px;"></div>
+  <div style="height: 20px;"></div>
+
+  <!-- review part -->
+  <div :id="`${path}#reviews`" style="height: 36px;"></div>
+  <div class="card" style="flex-direction: column;">
+    <t-space style="display: flex; width: 100%;">
+      <div>
+        <div class="title">REVIEWS</div>
+        <div class="line"></div>
+      </div>
+      <div style="margin-top: 30px; display: flex; justify-content: flex-end; margin-right:30px ;">
+        <t-button>评论</t-button>
+      </div>
+    </t-space>
+    <div>
+      <t-list :split="true">
+        <t-list-item v-for="(item, index) in commentsData" :key="index">
+          <template #content>
+            <div class="comment_card">
+              <div class="stars"><t-rate :default-value="4.5" allow-half disabled size="16" /></div>
+                <div class="comment_infos">
+                  <p class="date-time">
+                     {{item.datetime}}
+                  </p>
+                  <p class="description">
+                    {{ item.content }}
+                  </p>
+              </div>
+              <div class="author">
+                — {{ item.author }}
+              </div>
+            </div>
+          </template>
+        </t-list-item>
+      </t-list>
+    </div>
+  </div>
+
+
+  <div :id="`${path}#policy`" style="height: 60px;"></div>
+  <t-space style="display: flex; width: 100%; margin-left: 24px;">
+      <div>
+        <div class="title">ADMISSION POLICY</div>
+        <div class="line"></div>
+      </div>
+  </t-space>
+  <div class="ticket_card" style="margin-left: 55px; margin-top: -10px;">
+      <p style="  margin-top: 0.4rem; color: rgb(70, 73, 79);font-weight: 600;">
+        Admission Rules:
+      </p>
+      <p style="  margin-top: 0.4rem; line-height: 1.625;color: rgb(70, 73, 79);;">
+        1. 不允许携带食物或饮料进场
+        <br>
+        2. 不允许开闪光灯拍照
+        <br>
+        3. 演出48h前不予退款
+        <br>
+      </p>
+  </div>
+  <div style="height: 20px;"></div>
+  <div style="height: 20px;"></div>
+
 </template>
 
 <script setup>
-import { HeartIcon, HeartFilledIcon, CloudUploadIcon, StarIcon, StarFilledIcon, DiscountIcon } from 'tdesign-icons-vue-next';
+import { HeartIcon, HeartFilledIcon, ListIcon, TableIcon, StarFilledIcon, DiscountIcon } from 'tdesign-icons-vue-next';
 import { MessagePlugin } from 'tdesign-vue-next';
 import { NotifyPlugin, Link } from 'tdesign-vue-next';
 import { computed, getCurrentInstance, ref,onMounted,onUnmounted } from 'vue';
 import get from 'lodash/get';
 import { defineExpose,watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import EventTicketTable from './EventTicketTable.vue';
+import EventTicketCalender from './EventTicketCalender.vue';
 const value_lable = ref('events');
+
+const show_event_type = ref(true);
+
+const onClickEventType = (value) => {
+  show_event_type.value = value;
+};
 
 const onTabChange = (newValue) => {
   value_lable.value = newValue;
@@ -176,7 +305,7 @@ const commentsData = [
     avatar: 'https://tdesign.gtimg.com/site/avatar.jpg',
     author: '评论作者名B',
     datetime: '今天16:38',
-    content: '评论作者名B写的评论内容。',
+    content: '评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。评论作者名B写的评论内容。',
   },
   {
     id: 'C',
@@ -331,6 +460,72 @@ button#scrollButton.fixed {
   position: fixed;
   bottom: auto;
   top: 20px;
+}
+
+
+.title {
+  color: #5e6066;
+  font-size: 1.6em;
+  font-weight: 700;
+  margin: 30px;
+  letter-spacing: 3.5px;
+}
+
+.line {
+    width: 70px;
+    height: 4px;
+    margin-top: -15px;
+    margin-bottom: 20px;
+    margin-left: 30px;
+    background-color: black;
+  }
+
+
+  .comment_card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: rgba(255, 255, 255, 1);
+  padding: 20px;
+}
+
+.price_card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+}
+
+
+.stars {
+  display: flex;
+  grid-gap: 0.125rem;
+  gap: 0.125rem;
+  margin-bottom: -10px;
+  color: rgb(238, 203, 8);
+}
+
+.comment_infos {
+  margin-top: 1rem;
+}
+
+.date-time {
+  color: rgba(7, 63, 216, 1);
+  font-size: 12px;
+  font-weight: 600;
+}
+
+.description {
+  margin-top: 0.4rem;
+  line-height: 1.625;
+  color: rgba(107, 114, 128, 1);
+}
+
+.author {
+  margin-top: 1.3rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  color: rgba(107, 114, 128, 1);
 }
 
 </style>
