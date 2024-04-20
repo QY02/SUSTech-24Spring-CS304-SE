7b530eea5108b00ed0d625ea054f569afa7f8a52
ZhangYP21 <2826287819@qq.com>
2024-04-21 00:15:52 +0800

    系统通知前端界面

--- frontend/src/components/notification/SysNotification.vue
+++ frontend/src/components/notification/SysNotification.vue
@@ -13,13 +13,10 @@
     >
         <t-space direction="vertical" style="width: 100%">
             <t-collapse v-model="activeNames" accordion expand-mutex=true>
-              <!-- 使用 v-for 循环渲染折叠面板 -->
               <t-collapse-panel v-for="(item, index) in notifications" :key="index" :value="index.toString()">
-                <!-- 根据数组中的类型值动态设置折叠面板的标题 -->
                 <template #header>
                   {{ getHeaderByType(item.type) }}
                 </template>
-                <!-- 折叠面板展开或折叠的内容 -->
                 {{ item.content }}
               </t-collapse-panel>
             </t-collapse>
@@ -40,22 +37,20 @@ const visible = useVModel(props, 'visible', emit)
 
 // 模拟数据，包含通知信息和对应的类型
 const notifications = [
-  { type: 'start', content: 'start' },
-  { type: 'cancel', content: 'cancel' },
-  { type: 'subscribe', content: 'subscribe' },
-  { type: 'join', content: 'join' },
-  { type: 'modify', content: 'modify' }
+  { type: 0, content: 'start' },
+  { type: 1, content: 'cancel' },
+  { type: 2, content: 'modify' },
+  { type: 3, content: 'join' },
 ];
 
 // 当前展开的折叠面板名称
 const activeNames = ref([]);
-const HEADERS = {
-  start: '开始通知',
-  cancel: '取消通知',
-  subscribe: '订阅成功通知',
-  join: '参加成功通知',
-  modify: '活动修改通知'
-};
+const HEADERS = [
+  '开始通知',
+   '取消通知',
+  '参加成功通知',
+   '活动修改通知'
+];
 // 根据通知类型获取对应的标题
 const getHeaderByType = (type) => {
   return HEADERS[type] || '未知标题';
