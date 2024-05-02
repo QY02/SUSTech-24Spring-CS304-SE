4681b9d2cf7a484d7dd4c9509a154c77a9b96348
QY02 <1494893742@qq.com>
2024-05-02 21:44:15 +0800

    /orderRecord/getMyOrderRecordByEventId接口支持返回eventSession和event信息

--- backend/src/main/java/org/cs304/backend/controller/OrderRecordController.java
+++ backend/src/main/java/org/cs304/backend/controller/OrderRecordController.java
@@ -4,11 +4,9 @@ import com.alibaba.fastjson2.JSONObject;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.cs304.backend.entity.OrderRecord;
 import org.cs304.backend.service.IOrderRecordService;
 import org.cs304.backend.utils.Result;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +25,11 @@ public class OrderRecordController {
             ### 参数 ###
             mode = 0: 只返回eventSessionId,
             
-            mode = 1: 返回所有数据
+            mode = 1: 返回orderRecord的所有数据
+            
+            mode = 2: 返回orderRecord和eventSession的所有数据
+            
+            mode = 3: 返回orderRecord, eventSession和event的所有数据
             """)
     @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, content = @Content(examples = @ExampleObject("{\"eventId\": 1, \"mode\": 0}")))
     public Result getMyOrderRecordByEventId(HttpServletResponse response, HttpServletRequest request, @RequestBody JSONObject requestBody) {
@@ -36,6 +38,4 @@ public class OrderRecordController {
         Integer mode = requestBody.getInteger("mode");
         return Result.success(response, orderRecordService.getMyOrderRecordByEventId(userId, eventId, mode));
     }
-
-
 }
--- backend/src/main/java/org/cs304/backend/service/impl/OrderRecordServiceImpl.java
+++ backend/src/main/java/org/cs304/backend/service/impl/OrderRecordServiceImpl.java
@@ -1,9 +1,13 @@
 package org.cs304.backend.service.impl;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.cs304.backend.entity.OrderRecord;
 import org.cs304.backend.exception.ServiceException;
+import org.cs304.backend.mapper.EventMapper;
+import org.cs304.backend.mapper.EventSessionMapper;
 import org.cs304.backend.mapper.OrderRecordMapper;
 import org.cs304.backend.service.IOrderRecordService;
 import org.springframework.stereotype.Service;
@@ -14,17 +18,30 @@ import java.util.stream.Collectors;
 @Service
 public class OrderRecordServiceImpl extends ServiceImpl<OrderRecordMapper, OrderRecord> implements IOrderRecordService {
 
+    @Resource
+    private EventMapper eventMapper;
+    @Resource
+    private EventSessionMapper eventSessionMapper;
+
     @Override
     public Object getMyOrderRecordByEventId(String userId, Integer eventId, Integer mode) {
-        if ((eventId == null) || (mode == null) || ((mode != 0) && (mode != 1))) {
+        if ((eventId == null) || (mode == null) || ((mode != 0) && (mode != 1) && (mode != 2) && (mode != 3))) {
             throw new ServiceException("400", "Invalid data");
         }
         List<OrderRecord> orderRecordList = baseMapper.selectList(new QueryWrapper<OrderRecord>().eq("user_id", userId).eq("event_id", eventId));
-        if (mode == 0) {
-            return orderRecordList.stream().map(OrderRecord::getEventSessionId).collect(Collectors.toList());
-        }
-        else {
-            return orderRecordList;
-        }
+        return orderRecordList.stream().map(orderRecord -> {
+            if (mode == 0) {
+                return orderRecord.getEventSessionId();
+            } else if (mode == 1) {
+                return orderRecord;
+            } else {
+                JSONObject jsonObject = JSONObject.from(orderRecord);
+                jsonObject.put("eventSession", eventSessionMapper.selectById(orderRecord.getEventSessionId()));
+                if (mode == 3) {
+                    jsonObject.put("event", eventMapper.selectById(orderRecord.getEventId()));
+                }
+                return jsonObject;
+            }
+        }).collect(Collectors.toList());
     }
 }
