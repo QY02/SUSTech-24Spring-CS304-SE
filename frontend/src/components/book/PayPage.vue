fb7762291c028e1bc62b07005d538406f022a183
ultramarine <2549952274@qq.com>
2024-05-17 19:06:25 +0800

    支付前端+ 未测试

--- frontend/src/components/book/PayPage.vue
+++ frontend/src/components/book/PayPage.vue
@@ -1,29 +1,29 @@
 <template>
-    <t-descriptions title="预定内容" column="1">
-        <t-descriptions-item label="活动名称">{{eventDetail.name}}</t-descriptions-item>
-        <t-descriptions-item label="活动时间">{{ sessionInformation[bookingInformation.chosenSession].startTime }} - {{
-            sessionInformation[bookingInformation.chosenSession].endTime }}</t-descriptions-item>
-        <t-descriptions-item label="活动场地">{{ sessionInformation[bookingInformation.chosenSession].venue
-            }}</t-descriptions-item>
-        <t-descriptions-item label="座位">{{ bookingInformation.chosenSeat }}</t-descriptions-item>
-    </t-descriptions>
-    <t-descriptions-item label="价格">{{ bookingInformation.seatPrice }}</t-descriptions-item>
-    <t-space size="medium">
-        <t-button @click="currentStep--">上一步</t-button>
-        <t-button @click="prePay">前往付款</t-button>
-    </t-space>
+  <t-descriptions title="预定内容" column="1">
+    <t-descriptions-item label="活动名称">{{ eventDetail.name }}</t-descriptions-item>
+    <t-descriptions-item label="活动时间">{{ sessionInformation[bookingInformation.chosenSession].startTime }} - {{
+      sessionInformation[bookingInformation.chosenSession].endTime }}</t-descriptions-item>
+    <t-descriptions-item label="活动场地">{{ sessionInformation[bookingInformation.chosenSession].venue
+      }}</t-descriptions-item>
+    <t-descriptions-item label="座位">{{ bookingInformation.chosenSeat }}</t-descriptions-item>
+  </t-descriptions>
+  <t-descriptions-item label="价格">{{ bookingInformation.seatPrice }}</t-descriptions-item>
+  <t-space size="medium">
+    <t-button @click="currentStep--">上一步</t-button>
+    <t-button @click="prePay">前往付款</t-button>
+  </t-space>
 
 
 </template>
 <script setup lang="ts">
-import axios,  {AxiosRequestConfig} from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { currentStep, submitData } from '@/components/book/Steps.vue';
-import { onMounted, reactive, Ref, ref, watch } from "vue";
+import { onMounted, onUnmounted, reactive, Ref, ref, watch } from "vue";
 import { sessionInformation, bookingInformation } from '@/components/book/Steps.vue';
 import { MessagePlugin, NotifyPlugin } from "tdesign-vue-next";
 
 const eventDetail = ref({
-  name:''
+  name: ''
 })
 const getEventDetail = () => {
   axios.get(`/event/getEventDetail/${sessionStorage.getItem('eventId')}`, {
@@ -35,7 +35,6 @@ const getEventDetail = () => {
   }).catch(() => { })
 }
 getEventDetail();
-
 const result = ref(0);
 const orderId = ref(-1)
 const prePay = async () => {
@@ -48,20 +47,55 @@ const prePay = async () => {
       nameEng: item.nameEng,
       value: item.value
     })))
-  }, {headers: {token: sessionStorage.getItem('token')}} as AxiosRequestConfig).then((response) => {
+  }, { headers: { token: sessionStorage.getItem('token') } } as AxiosRequestConfig).then((response) => {
     orderId.value = response.data.data;
     MessagePlugin.success('提交支付信息成功');
     window.open(`http://localhost:5173/orderRecord/pay/${orderId}`)
+    startPolling();
     // currentStep.value++;
   })
-  .catch(error => {})
+    .catch(error => { })
 }
 
 const payResult = ref(0);
 
-function getPayResult(){
-  axios.post("/orderRecord/getPayResultById", { id:orderId }, 
-  {headers: {token: sessionStorage.getItem('token')}} as AxiosRequestConfig)
+let intervalId = null;
+let timeoutId = null;
+
+const startPolling = () => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+    timeoutId = null; // 清除后将timeoutId设为null
+  }
+    // 定义轮询函数
+  const poll = () => {
+    if (payResult.value === 1) { // 当外部条件满足时
+      clearInterval(intervalId); // 清除间隔定时器
+      return; // 结束轮询
+    }
+
+    // 每10秒请求一次
+    getPayResult(); // 执行轮询请求
+  };
+
+  // 每10秒请求一次
+  intervalId = setInterval(poll, 10000);
+  // 10分钟后停止轮询
+  timeoutId = setTimeout(() => {
+    clearInterval(intervalId);
+  }, 10 * 60 * 1000); // 10分钟
+};
+
+onUnmounted(() => {
+  if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
+      }
+});
+
+  function getPayResult() {
+    axios.post("/orderRecord/getPayResultById", { id: orderId },
+      { headers: { token: sessionStorage.getItem('token') } } as AxiosRequestConfig)
       .then(response => {
         payResult.value = response.data.data;
       })
