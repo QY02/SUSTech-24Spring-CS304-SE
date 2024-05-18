ae1bd9fcc646e4f4d9cd712e34ed87ad6229e51d
shadowfall09 <120352573+shadowfall09@users.noreply.github.com>
2024-05-18 23:16:36 +0800

    电脑卡死了，提交一下换台电脑写

--- backend/src/main/java/org/cs304/backend/controller/ChatMessageController.java
+++ backend/src/main/java/org/cs304/backend/controller/ChatMessageController.java
@@ -3,6 +3,7 @@ package org.cs304.backend.controller;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -29,7 +30,19 @@ public class ChatMessageController {
     @Resource
     IUserService userService;
 
+    /**
+     * This method is used to fetch chat messages between two users.
+     * It is mapped to the "/get/{toUserID}" endpoint and is invoked on HTTP POST requests.
+     *
+     * @param request  The HttpServletRequest object that contains the request the client made of the servlet.
+     * @param response The HttpServletResponse object that contains the response the servlet sends to the client.
+     * @param toUserID The ID of the user to whom the chat messages are to be fetched.
+     *
+     * @return Result object that contains the response code, message and a list of chat messages.
+     *
+     */
     @PostMapping("/get/{toUserID}")
+    @Operation(summary = "获取两个用户之间的聊天记录", description = "传入toUserID")
     public Result onLogin(@NotNull HttpServletRequest request, HttpServletResponse response, @PathVariable ("toUserID")String toUserID) {
         try {
             String userID = (String) request.getAttribute("loginUserId");
