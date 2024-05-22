5c950024fa16dea6ad76974fd31012e67c8bd235
2069958859 <12110141@mail.sustech.edu.cn>
2024-05-23 00:08:44 +0800

    定时功能测试正常

--- backend/src/main/java/org/cs304/backend/config/SpringConfig.java
+++ backend/src/main/java/org/cs304/backend/config/SpringConfig.java
@@ -2,9 +2,11 @@ package org.cs304.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableScheduling
 public class SpringConfig {
 
 }
