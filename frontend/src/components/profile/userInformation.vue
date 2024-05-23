1056fbcbd2de673509a3fc23f5a942116995ee02
ultramarine <2549952274@qq.com>
2024-05-23 22:48:41 +0800

    修改本人的userInformation

--- frontend/src/components/profile/userInformation.vue
+++ frontend/src/components/profile/userInformation.vue
@@ -1,10 +1,10 @@
 <template>
   <el-card :bordered="false" shadow v-if="!editYes"
-           style="display:flex;  align-items: center; justify-content: center; flex-direction: row ;padding-left: 30px; padding-right:30px; margin: 30px; height: 50vh;">
+    style="display:flex;  align-items: center; justify-content: center; flex-direction: row ;padding-left: 30px; padding-right:30px; margin: 30px; height: 65vh;">
     <br>
     <div style="width: 100%;">
       <t-space align="center" :size="40">
-        <t-avatar :image="avatar" size="100px"/>
+        <t-avatar :image="avatar" size="100px" />
         <t-descriptions title="个人信息" :column="1">
           <t-descriptions-item label="名字">{{ formData.name }}</t-descriptions-item>
           <br>
@@ -27,12 +27,7 @@
         修改个人信息
       </t-button>
       <t-popup content="退出登录">
-        <t-button
-            style="position: absolute; right: 50px; top: 50px"
-            theme="default"
-            variant="base"
-            @click="logout"
-        >
+        <t-button style="position: absolute; right: 50px; top: 50px" theme="default" variant="base" @click="logout">
           <logout-icon></logout-icon>
         </t-button>
       </t-popup>
@@ -42,17 +37,18 @@
   </el-card>
 
   <el-card :bordered="false" shadow v-else
-           style=" align-items: center; justify-content: center; height: 57%; margin: 30px;">
+    style=" align-items: center; justify-content: center; height: 85vh; margin: 30px;">
     <br>
     <br>
     <t-form ref="form" :data="formData" :rules="FORM_RULES" reset-type="initial" colon @reset="onReset"
-            @submit="onSubmit" style="width: 80%; margin-left: 10%;">
+      @submit="onSubmit" style="width: 80%; margin-left: 10%;">
       <t-form-item label="姓名" name="name">
         <t-input v-model="formData.name"></t-input>
       </t-form-item>
 
       <t-form-item label="学院" name="department">
-        <t-input v-model="formData.department"></t-input>
+        <t-select v-model="formData.department" :options="options1">
+        </t-select>
       </t-form-item>
 
       <t-form-item label="邮箱" name="email">
@@ -63,9 +59,8 @@
       <t-form-item label="电话" name="phoneNumber">
         <t-input v-model="formData.phoneNumber"></t-input>
       </t-form-item>
-      <t-form-item label="感兴趣的活动类型" name="favType" :label-width="140">
+      <t-form-item label="感兴趣的活动类型" name="favTypes" :label-width="140">
         <t-checkbox-group v-model="formData.favTypes" :options="EVENT_TYPE_value_1">
-
         </t-checkbox-group>
       </t-form-item>
 
@@ -73,7 +68,7 @@
       <t-form-item label="头像" name="avatar">
         <t-space :size="30">
           <t-image :src="avatarList[formData.avatar]" v-model="formData.avatar"
-                   :style="{ width: '120px', height: '120px' }"/>
+            :style="{ width: '120px', height: '120px' }" />
           <t-button @click="() => visibleAvator = true">修改头像</t-button>
         </t-space>
         <!-- <t-upload v-model="formData.avatar"
@@ -84,12 +79,12 @@
 
       <t-form-item>
         <t-space :size="20">
-          <t-button theme="default" variant="base" @click="() => {
+          <t-button theme="default" variant="base" :disabled="loadingg" @click="() => {
             editYes = !editYes;
           }">
             取消
           </t-button>
-          <t-button theme="default" variant="base" type="reset">重置</t-button>
+          <t-button theme="default" variant="base" type="reset" :disabled="load">重置</t-button>
           <t-button theme="success" type="submit" :loading="loadingg">提交</t-button>
         </t-space>
       </t-form-item>
@@ -98,12 +93,12 @@
   </el-card>
   <ChangeEmail v-model:visible="visibleEmail" v-model:old_email="formData.email"></ChangeEmail>
   <t-dialog v-model:visible="visibleAvator" attach="body" header="修改头像" destroy-on-close:true width="600px"
-            :cancel-btn=null :confirm-btn=null>
+    :cancel-btn=null :confirm-btn=null>
     <template #body>
       <t-space v-for="(item, index) in avatarList" size="20px">
         <div style="margin: 20px;">
           <t-space direction="vertical">
-            <t-image :src="item" :style="{ width: '120px', height: '120px' }"/>
+            <t-image :src="item" :style="{ width: '120px', height: '120px' }" />
             <t-tag shape="mark" theme="primary" variant="light">选项{{ index + 1 }}</t-tag>
           </t-space>
         </div>
@@ -129,24 +124,15 @@
 <script setup>
 import ChangePassword from "@/components/profile/ChangePassword.vue";
 import ChangeEmail from "./ChangeEmail.vue";
-import {ref, onMounted, reactive} from "vue";
+import { ref, onMounted, reactive } from "vue";
 import axios from "axios";
-import {MessagePlugin} from 'tdesign-vue-next';
+import { MessagePlugin } from 'tdesign-vue-next';
 import router from "@/routers/index.js";
-import {LogoutIcon} from "tdesign-icons-vue-next";
-import {EVENT_TYPE_MAP, EVENT_TYPE_value_1} from "../../constants/index.js";
-import {form} from "../book/InputInformation.vue";
+import { LogoutIcon } from "tdesign-icons-vue-next";
+import { EVENT_TYPE_MAP, EVENT_TYPE_value_1 } from "../../constants/index.js";
+import { form } from "../book/InputInformation.vue";
 
 const avatar = ref('https://tdesign.gtimg.com/site/avatar.jpg')
-const avatarMap = {
-  'https://avatars.githubusercontent.com/pengyyyyy': 1,
-  'https://tdesign.gtimg.com/site/avatar.jpg': 2,
-  'https://avatars.githubusercontent.com/LeeJim': 3,
-  'https://avatars.githubusercontent.com/chaishi': 4,
-  'https://avatars.githubusercontent.com/pattybaby110': 5,
-  'https://avatars.githubusercontent.com/u/7361184?v=4': 6
-  // 可以添加更多的映射关系
-}
 const avatarList = ['https://avatars.githubusercontent.com/pengyyyyy',
   'https://tdesign.gtimg.com/site/avatar.jpg',
   'https://avatars.githubusercontent.com/LeeJim',
@@ -189,31 +175,30 @@ axios.post(`/user/get/${sessionStorage.getItem('uid')}`, {}, {
     token: sessionStorage.getItem('token'),
   },
 })
-    .then((response) => {
-      info.value = response.data.data;
-      console.log(info.value.iconId);
-      avatar.value = avatarList[info.value.iconId];
-      formData.avatar = info.value.iconId;
-      formData.name = info.value.name;
-      formData.email = info.value.email;
-      formData.phoneNumber = info.value.phoneNumber;
-      formData.department = info.value.department;
-    })
-    .catch(() => {
-    })
+  .then((response) => {
+    info.value = response.data.data;
+    console.log(info.value.iconId);
+    avatar.value = avatarList[info.value.iconId];
+    formData.avatar = info.value.iconId;
+    formData.name = info.value.name;
+    formData.email = info.value.email;
+    formData.phoneNumber = info.value.phoneNumber;
+    formData.department = info.value.department;
+  })
+  .catch(() => {
+  })
 const FORM_RULES = ref({
-  name: [{required: true, message: '名字不可为空'}],
+  name: [{ required: true, message: '名字不可为空' }],
   favTypes: [{
     required: true,
     message: '请选择1-12个感兴趣的活动类型',
-  }
-  ],
+  }],
   phoneNumber: [
-    {telnumber: true, message: '请输入正确的手机号码'}
+    { telnumber: true, message: '请输入正确的手机号码' }
   ],
-  department: [{required: true, message: '学院不可为空'},],
-  email: [{required: true, message: '学院不可为空'},],
-
+  department: [{ required: true, message: '学院不可为空' },],
+  email: [{ required: true, message: '学院不可为空' },],
+  avatar: [{ required: true, message: '头像不可为空' },]
 });
 let temp = ref();
 
@@ -224,24 +209,30 @@ axios.post(`/user/getUserFavoriteType`, {
     token: sessionStorage.getItem('token'),
   },
 })
-    .then((response) => {
-      favType.value = response.data.data;
-      temp.value = JSON.stringify(favType.value)
-      // alert(temp.value)
+  .then((response) => {
+    favType.value = response.data.data;
+    temp.value = JSON.stringify(favType.value)
+    // alert(temp.value)
 
-      temp.value = temp.value.substring(1, temp.value.length - 1)
-      temp.value = temp.value.split(',')
-      temp.value = temp.value.map(Number);
-      // alert(temp.value)
-      formData.favTypes = temp.value
-      temp.value = temp.value.map(type => EVENT_TYPE_MAP[type]).join(', ')
+    temp.value = temp.value.substring(1, temp.value.length - 1)
+    temp.value = temp.value.split(',')
+    temp.value = temp.value.map(Number);
+    // alert(temp.value)
+    formData.favTypes = temp.value
+    temp.value = temp.value.map(type => EVENT_TYPE_MAP[type]).join(', ')
 
-      // alert(typeof temp[0])
-      // alert(temp.value)
-    })
-    .catch(() => {
-    })
+    // alert(typeof temp[0])
+    // alert(temp.value)
+  })
+  .catch(() => {
+  })
+  
+  const options1 = [
+  { label: '计算机系', value: '计算机系' },
+  { label: '物理系', value: '物理系' },
+  { label: '数学系', value: '数学系' },
 
+];
 
 const formData = reactive({
   name: '',
@@ -249,6 +240,7 @@ const formData = reactive({
   phoneNumber: '',
   department: '',
   avatar: '',
+  favTypes: '',
 });
 
 
@@ -260,6 +252,7 @@ const submitInfo = () => {
   loadingg.value = true
   axios.put(`/user/update`, {
     "id": sessionStorage.getItem('uid'),
+    "name": formData.name,
     "phoneNumber": formData.phoneNumber,
     "department": formData.department,
     "iconId": formData.avatar,
@@ -276,21 +269,22 @@ const submitInfo = () => {
         token: sessionStorage.getItem('token'),
       },
     })
-        .then(() => {
-          MessagePlugin.success('提交成功');
-          console.log(formData.avatar)
-          editYes.value = false;
-          loadingg.value = false;
-        })
-        .catch(error => {
-        });
+      .then(() => {
+        MessagePlugin.success('提交成功');
+        temp.value = formData.favTypes.map(type => EVENT_TYPE_MAP[type]).join(', ')
+        console.log(formData.avatar)
+        editYes.value = false;
+        loadingg.value = false;
+      })
+      .catch(error => {
+      });
 
   }).catch(() => {
     loadingg.value = false;
   })
 }
 
-const onSubmit = ({validateResult, firstError}) => {
+const onSubmit = ({ validateResult, firstError }) => {
   // alert(formData.favTypes)
   if (validateResult === true) {
     if (formData.favTypes.length <= 0 || formData.favTypes.length > 12) {
@@ -356,8 +350,11 @@ const editYes = ref(false);
 
 .button-container {
   display: flex;
-  justify-content: space-around; /* 按钮之间平均分配空间 */
-  align-items: center; /* 垂直居中对齐按钮 */
-  margin-top: 20px; /* 与上面的组件保持一定距离 */
+  justify-content: space-around;
+  /* 按钮之间平均分配空间 */
+  align-items: center;
+  /* 垂直居中对齐按钮 */
+  margin-top: 20px;
+  /* 与上面的组件保持一定距离 */
 }
 </style>
